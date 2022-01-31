63a0d01921fc6a73382ec805fec06df78c3d6261
Jagat Jeeban <jagatjeebanmaharana@gmail.com>
2022-01-31 19:47:18 +0530

    Filter the results
    1.Update the MarsApiService
    2.Update the overview viewModel
    3.Connect the fragment to options menu

--- app/src/main/java/com/example/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/marsrealestate/network/MarsApiService.kt
@@ -7,6 +7,14 @@ import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
+
+//define constants that match the query values the web service expects.
+enum class MarsApiFilter(val value:String){
+    SHOW_RENT("rent"),
+    SHOW_BUY("buy"),
+    SHOW_ALL("all")
+}
 
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
 
@@ -24,7 +32,7 @@ private val retrofit = Retrofit.Builder()
 //interface that defines how retrofit talks to the web server using http requests.
 interface MarsApiService{
     @GET("realestate")
-    suspend fun getProperties():List<MarsProperty>
+    suspend fun getProperties(@Query("filter") type:String):List<MarsProperty>
 }
 //initializing a retrofit service object
 object MarsApi{
