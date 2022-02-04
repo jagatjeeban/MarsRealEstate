21b89d4c2be6bb1a98f7b23e72fb0bd11c0e65d9
Jagat Jeeban <jagatjeebanmaharana@gmail.com>
2022-02-04 13:29:42 +0530

    Create a more useful detail page

--- app/src/main/java/com/example/marsrealestate/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/marsrealestate/detail/DetailViewModel.kt
@@ -1,10 +1,8 @@
 package com.example.marsrealestate.detail
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.example.marsrealestate.R
 import com.example.marsrealestate.network.MarsProperty
 
 /**
@@ -18,5 +16,30 @@ class DetailViewModel(marsProperty: MarsProperty, app: Application) : AndroidVie
 
     init {
         _selectedProperty.value = marsProperty
+    }
+
+    /**
+     * This transformation tests whether the selected property is a rental.If the property is a rental,
+     * the transformation chooses the appropriate string from the resources with a Kotlin when {} switch.
+     */
+    val displayPropertyPrice = Transformations.map(selectedProperty){
+        app.applicationContext.getString(
+            when(it.isRental){
+                true -> R.string.display_price_monthly_rental
+                false -> R.string.display_price
+            }, it.price )
+    }
+
+    /**
+     * This transformation concatenates multiple string resources,
+     * based on whether the property type is a rental.
+     */
+    val displayPropertyType = Transformations.map(selectedProperty){
+        app.applicationContext.getString(R.string.display_type,
+        app.applicationContext.getString(
+            when(it.isRental){
+                true -> R.string.type_rent
+                false -> R.string.type_sale
+            }))
     }
 }
